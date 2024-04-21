fdb1d956c97e95ee13b9a362c95cd7517cff271b
Hilll19 <hilworking0110@gmail.com>
2024-04-21 16:20:19 +0700

    try vercel change

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
             <p className='text-[#00df9a] font-boldp-2'>MONITORING YOUR KIDS</p>
             <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Guard with SenDigi</h1>
             <div className='flex justify-center items-center'>
-                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Secure, Real-Time, Trusted</p>
+                <p className='md:text-5xl sm:text-4xl text-xl font-bold py-4'>Real-Time, Trusted, Secured</p>
                 <ReactTyped className='md:text-5xl sm:text-4xl text-xl font-bold md:pl-4 pl-2' strings={['Family', 'Parents', 'Kids']} typeSpeed={120} backSpeed={146} loop/>
             </div>
             <p className='md:text-2zl text-xl font-bold text-gray-500'>Monitoring your digital activity kids to guard from negatife effect gadget </p>
