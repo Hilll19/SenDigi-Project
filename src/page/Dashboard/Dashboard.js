83d3c20b85522fb0ce06d4fbc7bc726e06dbf9b9
Hilll19 <hilworking0110@gmail.com>
2024-04-28 22:03:01 +0700

    Fix page TimeUsage and Add list app in dashboard

--- src/page/Dashboard/Dashboard.js
+++ src/page/Dashboard/Dashboard.js
@@ -1,107 +1,144 @@
-import React, { useState } from 'react';
-import { Line } from 'react-chartjs-2';
+import React, { useEffect, useState } from "react";
+import { Line } from "react-chartjs-2";
 // import Navbar from '../../components/Navbar';
-import Sidebar from '../../components/SideBar';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faWhatsapp, faInstagram, faFacebook, faTiktok, faTwitter, faLinkedin, faSnapchat, faPinterest, faReddit, faTumblr } from '@fortawesome/free-brands-svg-icons';
+import Sidebar from "../../components/SideBar";
 
 const Dashboard = () => {
-    const chartData = {
-        labels: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
-        datasets: [
-            {
-                label: 'Time Usage',
-                data: [65, 59, 80, 81, 56, 55],
-                fill: false,
-                borderColor: '#00df9a',
-            },
-            {
-                label: 'Lock App',
-                data: [28, 48, 40, 19, 86, 27],
-                fill: false,
-                borderColor: '#ff0000',
-            },
-            {
-                label: 'Scheduling',
-                data: [35, 45, 67, 28, 43, 30],
-                fill: false,
-                borderColor: '#0000ff',
-            },
-        ],
-    };
+  const [appList, setAppList] = useState([]);
+  const [chartData, setChartData] = useState({
+    labels: [],
+    datasets: [],
+  });
 
-    const [appList, setAppList] = useState([
-        { name: 'WhatsApp', locked: true },
-        { name: 'Instagram', locked: false },
-        { name: 'Facebook', locked: true },
-        { name: 'TikTok', locked: false },
-        { name: 'Twitter', locked: true },
-        { name: 'LinkedIn', locked: false },
-        { name: 'Snapchat', locked: true },
-        { name: 'Pinterest', locked: false },
-        { name: 'Reddit', locked: true },
-        { name: 'Tumblr', locked: false },
-    ]);
+  useEffect(() => {
+    fetchData();
+  }, []);
 
-    const toggleLockStatus = (index) => {
-        const updatedAppList = [...appList];
-        updatedAppList[index].locked = !updatedAppList[index].locked;
-        setAppList(updatedAppList);
-    };
+  const fetchData = () => {
+    fetch("http://localhost:8888/api/apps", {
+      credentials: "include", // If needed
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const apps = data.data.map((app) => ({
+          name: app.Name,
+          locked: app.LockStatus,
+          icon: app.Icon,
+          timeUsage: app.TimeUsage,
+        }));
 
-    return (
-        <div className="flex flex-col h-screen">
-            {/* <Navbar /> */}
-            <div className="flex flex-col flex-1 md:flex-row">
-                <Sidebar />
-                <div className="flex-grow p-4 md:p-10">
-                    <h1 className="text-3xl font-bold mb-6 text-white">Dashboard</h1>
-                    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h2 className="text-xl font-bold mb-4">Time Usage</h2>
-                            <Line data={chartData} />
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h2 className="text-xl font-bold mb-4">Lock App</h2>
-                            {/* <Line data={chartData} /> */}
-                        </div>
-                        <div className="bg-white p-4 rounded-lg shadow-md">
-                            <h2 className="text-xl font-bold mb-4">Scheduling</h2>
-                            {/* <Line data={chartData} /> */}
-                        </div>
-                    </div>
-                    <div className="mt-8 bg-white p-4 rounded-lg shadow-md overflow-y-auto max-h-96">
-                        <h2 className="text-xl font-bold mb-4">App List</h2>
-                        <ul>
-                            {appList.map((app, index) => (
-                                <li key={index} className="flex items-center justify-between py-2 border-b">
-                                    <div className="flex items-center">
-                                        {app.name === 'WhatsApp' && <FontAwesomeIcon size='2x' icon={faWhatsapp} className="mr-2" />}
-                                        {app.name === 'Instagram' && <FontAwesomeIcon size='2x' icon={faInstagram} className="mr-2" />}
-                                        {app.name === 'Facebook' && <FontAwesomeIcon size='2x' icon={faFacebook} className="mr-2" />}
-                                        {app.name === 'TikTok' && <FontAwesomeIcon size='2x' icon={faTiktok} className="mr-2" />}
-                                        {app.name === 'Twitter' && <FontAwesomeIcon size='2x' icon={faTwitter} className="mr-2" />}
-                                        {app.name === 'LinkedIn' && <FontAwesomeIcon size='2x' icon={faLinkedin} className="mr-2" />}
-                                        {app.name === 'Snapchat' && <FontAwesomeIcon size='2x' icon={faSnapchat} className="mr-2" />}
-                                        {app.name === 'Pinterest' && <FontAwesomeIcon size='2x' icon={faPinterest} className="mr-2" />}
-                                        {app.name === 'Reddit' && <FontAwesomeIcon size='2x' icon={faReddit} className="mr-2" />}
-                                        {app.name === 'Tumblr' && <FontAwesomeIcon size='2x' icon={faTumblr} className="mr-2" />}
-                                        <span>{app.name}</span>
-                                    </div>
-                                    <div 
-                                        className={`w-8 h-4 rounded-full bg-${app.locked ? 'red' : 'green'}-500 flex items-center justify-center cursor-pointer`}
-                                        onClick={() => toggleLockStatus(index)}
-                                    >
-                                        <div className={`w-3 h-3 rounded-full bg-white transform transition-transform duration-300 ease-in-out ${app.locked ? '' : 'translate-x-full'}`}></div>
-                                    </div>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                </div>
+        // Sort the apps based on timeUsage in descending order
+        apps.sort((a, b) => b.timeUsage - a.timeUsage);
+
+        setAppList(apps);
+
+        const convertedData = apps.map((app) => ({
+          icon: app.icon,
+          hour: Math.floor(app.timeUsage / 60),
+          minute: app.timeUsage % 60,
+        }));
+
+        const chartLabels = convertedData.map((app) => ({
+          icon: app.icon,
+          label: `${app.hour}h ${app.minute}m`,
+        }));
+
+        setChartData({
+          labels: chartLabels.map((item) => item.label),
+          datasets: [
+            {
+              label: "Time Usage",
+              data: chartLabels.map((item) => item.hour),
+              fill: false,
+              borderColor: "#00df9a",
+            },
+          ],
+        });
+      })
+      .catch((error) => console.error("Error fetching app data:", error));
+  };
+
+  return (
+    <div className="flex flex-col h-screen">
+      {/* <Navbar /> */}
+      <div className="flex flex-col flex-1 md:flex-row">
+        <Sidebar />
+        <div className="flex-grow p-4 md:p-10">
+          <h1 className="text-3xl font-bold mb-6 text-white">Dashboard</h1>
+          <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <h2 className="text-xl font-bold mb-4">Time Usage</h2>
+              <Line data={chartData} />
             </div>
+            <div className="bg-white p-4 rounded-lg shadow-md">
+              <h2 className="text-xl font-bold mb-4">Scheduling</h2>
+              {/* <Line data={chartData} /> */}
+            </div>
+            <div className="bg-white p-4 rounded-lg shadow-md overflow-y-auto max-h-60">
+              <h2 className="text-xl font-bold mb-4">Lock App</h2>
+              <ul>
+                {appList.map((app, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center justify-between py-2 border-b"
+                  >
+                    <div className="flex items-center">
+                      {app.icon && (
+                        <img
+                          src={`data:image/png;base64, ${app.icon}`}
+                          alt={app.name}
+                          className="h-8 w-8 mr-2"
+                        />
+                      )}
+                      <span>{app.name}</span>
+                    </div>
+                    <div>
+                      {app.locked ? (
+                        <div className="w-3 h-3 rounded-full bg-red-500"></div>
+                      ) : (
+                        <div className="w-3 h-3 rounded-full bg-green-500"></div>
+                      )}
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
+          <div className="mt-8 bg-white p-4 rounded-lg shadow-md">
+            <h2 className="text-xl font-bold mb-4">App List</h2>
+            <div className="overflow-y-auto max-h-96 overflow-x-hidden">
+              <ul>
+                {appList.map((app, index) => (
+                  <li
+                    key={index}
+                    className="flex items-center justify-between py-2 border-b"
+                  >
+                    <div className="flex items-center">
+                      {app.icon && (
+                        <img
+                          src={`data:image/png;base64, ${app.icon}`}
+                          alt={app.name}
+                          className="h-8 w-8 mr-2"
+                        />
+                      )}
+                      <span>{app.name}</span>
+                    </div>
+                    <div>
+                      <div
+                        className={`w-3 h-3 rounded-full bg-white transform transition-transform duration-300 ease-in-out ${
+                          app.locked ? "" : "translate-x-full"
+                        }`}
+                      ></div>
+                    </div>
+                  </li>
+                ))}
+              </ul>
+            </div>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
--- src/page/TimeUsage.js
+++ src/page/TimeUsage.js
@@ -1,10 +1,8 @@
 import React, { useState, useEffect } from "react";
 import Navbar from "../components/Navbar";
-import { FaFacebookSquare, FaInstagram, FaTwitterSquare } from "react-icons/fa";
-import styled, { keyframes } from "styled-components"; // Tambahkan import ini
-import Chart from "chart.js/auto"; 
+import styled, { keyframes } from "styled-components";
+import Chart from "chart.js/auto";
 
-// Animasi fadeInUp
 const fadeInUpAnimation = keyframes`
   from {
     opacity: 0;
@@ -20,32 +18,45 @@ const AnimatedDiv = styled.div`
   animation: 1s ${fadeInUpAnimation};
 `;
 
-const usageStatistics = [
-  { name: "Facebook", dailyHour: 2.5 },
-  { name: "Instagram", dailyHour: 1.8 },
-  { name: "Twitter", dailyHour: 3.2 },
-  { name: "Twitter", dailyHour: 3.2 },
-  { name: "Twitter", dailyHour: 3.2 },
-  // Add more app data as needed
-];
-
 function TimeUsage() {
-  const [showAnimation, setShowAnimation] = useState(false);
+  const [appList, setAppList] = useState([]);
 
   useEffect(() => {
-    setShowAnimation(true);
+    fetchData();
+  }, []);
 
+  const fetchData = () => {
+    fetch("http://localhost:8888/api/apps", {
+      credentials: "include",
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const apps = data.data.map((app) => ({
+          name: app.Name,
+          dailyHour: app.TimeUsage / 60, // Convert minutes to hours
+          icon: `data:image/png;base64, ${app.Icon}`, // Use base64 image from endpoint
+        }));
+
+        // Sort the apps based on dailyHour in descending order
+        apps.sort((a, b) => b.dailyHour - a.dailyHour);
+
+        setAppList(apps);
+      })
+      .catch((error) => console.error("Error fetching app data:", error));
+  };
+
+  useEffect(() => {
     // Initialize daily chart
     const dailyCtx = document.getElementById("dailyChart").getContext("2d");
     const dailyChart = new Chart(dailyCtx, {
       type: "bar",
       data: {
-        labels: usageStatistics.map((app) => app.name),
+        labels: appList.map((app) => app.name),
         datasets: [
           {
             label: "Daily Usage (hours)",
-            data: usageStatistics.map((app) => app.dailyHour),
-            backgroundColor: ["red", "blue", "green"], // Add colors as needed
+            data: appList.map((app) => app.dailyHour.toFixed(2)),
+            backgroundColor: "blue",
             borderWidth: 1,
           },
         ],
@@ -63,50 +74,21 @@ function TimeUsage() {
     return () => {
       dailyChart.destroy();
     };
-  }, []);
-
-  const selectIcon = (appName) => {
-    switch (appName) {
-      case "Facebook":
-        return <FaFacebookSquare size={35} />;
-      case "Instagram":
-        return <FaInstagram size={35} />;
-      case "Twitter":
-        return <FaTwitterSquare size={35} />;
-      default:
-        return null;
-    }
-  };
-
-  const calculateDailySummary = (usageData) => {
-    const totalDailyHours = usageData.reduce(
-      (acc, curr) => acc + curr.dailyHour,
-      0
-    );
-    return totalDailyHours.toFixed(2);
-  };
-
-  const calculateWeeklySummary = (usageData) => {
-    const totalWeeklyHours = calculateDailySummary(usageData) * 7;
-    return totalWeeklyHours.toFixed(2);
-  };
-
-  const calculateMonthlySummary = (usageData) => {
-    const totalMonthlyHours = calculateDailySummary(usageData) * 30;
-    return totalMonthlyHours.toFixed(2);
-  };
+  }, [appList]);
 
   const renderUsageStatistics = () => {
-    return usageStatistics.map((app, index) => (
+    return appList.map((app, index) => (
       <AnimatedDiv
         key={index}
         className="bg-white p-4 rounded-lg shadow-md mb-4"
       >
         <div className="flex items-center mb-2">
-          <div className="mr-2">{selectIcon(app.name)}</div>
+          <div className="mr-2">
+            <img src={app.icon} alt={app.name} width="35" />
+          </div>
           <h2 className="text-lg font-semibold">{app.name}</h2>
         </div>
-        <p>Daily Hour: {app.dailyHour}</p>
+        <p>Daily Hour: {app.dailyHour.toFixed(2)}</p>
       </AnimatedDiv>
     ));
   };
@@ -116,23 +98,15 @@ function TimeUsage() {
       <Navbar />
       <div className="container mx-auto mt-10">
         <h1 className="text-2xl font-bold mb-4 text-white">
-        Application Usage Statistics
+          Application Usage Statistics
         </h1>
         <div className="grid grid-cols-2 gap-4">
           <div className="bg-white p-4 rounded-lg shadow-md">
             <h2 className="text-lg font-semibold mb-2">Analyze Data</h2>
             <div className="bg-white p-4 rounded-lg shadow-md">
               <h2 className="text-lg font-semibold mb-2">Time</h2>
-              {/* Tampilkan summary analisis time usage */}
-              <p>Daily Summary: {calculateDailySummary(usageStatistics)}</p>
-              <p>Weekly Summary: {calculateWeeklySummary(usageStatistics)}</p>
-              <p>Monthly Summary: {calculateMonthlySummary(usageStatistics)}</p>
               <div className="grid grid-cols-2 gap-4 mt-4">
                 <canvas id="dailyChart"></canvas>
-                {/* Add canvas for weekly chart */}
-                <canvas id="weeklyChart"></canvas>
-                {/* Add canvas for monthly chart */}
-                <canvas id="monthlyChart"></canvas>
               </div>
             </div>
           </div>
@@ -141,10 +115,9 @@ function TimeUsage() {
             style={{ maxHeight: "400px", overflowY: "auto" }}
           >
             <h2 className="text-lg font-semibold mb-2">App Usage</h2>
-            {showAnimation && renderUsageStatistics()}
+            {renderUsageStatistics()}
           </div>
         </div>
-        {/* Add canvas elements for weekly and monthly charts */}
       </div>
     </div>
   );
