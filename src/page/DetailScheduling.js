0b5a62e6400feeb869481053321382a38a985075
Hilll19 <hilworking0110@gmail.com>
2024-04-28 14:48:05 +0700

    Add image and page schedulingdetail, also fix some text in detaillock

--- src/page/DetailScheduling.js
+++ src/page/DetailScheduling.js
@@ -1,36 +1,41 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Navbar from "../components/Navbar";
-import lockPic from "../assets/lockPic.jpg";
-import lockPic1 from "../assets/lockPic1.png";
-import lockPic2 from "../assets/lockPic2.png";
+import SchedulingPic from "../assets/schedulePic.jpg";
+import schedulePic1 from "../assets/schedulingPic1.png";
+import schedulePic2 from "../assets/schedulingPic2.png";
 import dashboardPic from "../assets/dashboardPic.png";
 
-function DetailLock() {
+function DetailScheduling() {
   return (
     <div>
       <Navbar />
       <div className="w-full bg-white py-16 px-4">
         <div className="max-w-[1240px] mx-auto">
           <h1 className="text-[#00df9a] font-bold text-center text-4xl mb-8">
-            Lock App feature
+            Scheduling Lock App feature
           </h1>
           <div className="w-full bg-white py-16 px-4">
             <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
-              <img className="w-[500PX] mx-auto my-4" src={lockPic} alt="/" />
+              <img
+                className="w-[500PX] mx-auto my-4"
+                src={SchedulingPic}
+                alt="/"
+              />
               <div className="flex flex-col justify-center">
                 <p className="md:text-4xl sm:text-3xl text-2xl font-bold py-2">
-                  Lock App Feature in SenDigi
+                  Scheduling Lock App in SenDigi
                 </p>
                 <p>
-                  The Lock App feature in SenDigi empowers parents with the
-                  ability to remotely lock specific applications on their
-                  children's devices. This feature provides parents with
-                  enhanced control over their children's digital activities,
-                  allowing them to regulate access to certain apps, thereby
-                  promoting a healthier balance between screen time and other
-                  activities. With SenDigi, parents can ensure a safer and more
-                  productive digital experience for their children
+                  The Scheduling Lock App feature in SenDigi empowers parents
+                  with the ability to remotely schedule locking and unlocking of
+                  specific applications on their children's devices. This
+                  feature provides parents with enhanced control over their
+                  children's digital activities, allowing them to regulate
+                  access to certain apps at predetermined times. By scheduling
+                  app locks, parents can promote a healthier balance between
+                  screen time and other activities, ensuring a safer and more
+                  productive digital experience for their children with SenDigi.
                 </p>
               </div>
             </div>
@@ -45,41 +50,33 @@ function DetailLock() {
           <div className="w-full bg-white py-16 px-4 rounded-lg">
             <div className="max-w-[1240px] mx-auto grid md:grid-cols-2">
               <p className="md:text-4xl sm:text-3xl text-2xl font-bold py-2 pt-8">
-                Lock App Benefit
+                Scheduling Lock <br />
+                App Benefit
               </p>
               <ol className="list-decimal pt-8">
                 <li>
-                  <strong>Comprehensive Parental Monitoring:</strong> The Lock
-                  App feature offers a comprehensive parental monitoring
-                  solution. It provides a platform accessible via web and mobile
-                  devices, empowering parents with robust tools to monitor and
-                  control their children's online activities in real-time.
+                  <strong>Comprehensive Parental Monitoring:</strong> Empower
+                  parents with robust tools for real-time monitoring and control
+                  of children's online activities.
                 </li>
                 <li>
-                  <strong>Flexibility:</strong> Parents gain flexibility with
-                  the Lock App feature. They can monitor and control their
-                  children's gadget usage across multiple devices, ensuring a
-                  seamless experience regardless of the device being used.
+                  <strong>Flexibility:</strong> Easily manage children's gadget
+                  usage across multiple devices for a seamless experience.
                 </li>
                 <li>
-                  <strong>Real-Time Monitoring and Control:</strong> With
-                  real-time monitoring and control functionalities, parents can
-                  promptly respond to any concerns or inappropriate activities,
-                  fostering a safer digital environment for their children.
+                  <strong>Real-Time Monitoring and Control:</strong> Promptly
+                  respond to concerns or inappropriate activities, fostering a
+                  safer digital environment.
                 </li>
                 <li>
-                  <strong>Customized Access Control:</strong> Parents can
-                  personalize access control profiles for each child, tailoring
-                  the restrictions to match their individual needs and
-                  developmental stages, thereby ensuring a safer online
-                  experience.
+                  <strong>Customized Access Control:</strong> Tailor access
+                  restrictions to match each child's individual needs and
+                  developmental stages.
                 </li>
                 <li>
-                  <strong>Insightful Usage Reports:</strong> The Lock App
-                  feature enables parents to access comprehensive reports on
-                  their children's online activities, providing valuable
-                  insights into usage patterns and helping parents make informed
-                  decisions about their children's digital habits.
+                  <strong>Insightful Usage Reports:</strong> Access
+                  comprehensive reports on children's online activities for
+                  informed decision-making.
                 </li>
               </ol>
             </div>
@@ -115,41 +112,38 @@ function DetailLock() {
           <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
             <img
               className="w-30 h-80 mx-auto mt-[-3rem] bg-white"
-              src={lockPic1}
+              src={schedulePic1}
               alt="#"
             />
             <h2 className="text-3xl font-bold text-center py-6">
-              Statistic Analyze Data
+              Scheduling Lock App
             </h2>
             <div className="text-center font-medium">
               <p className="mx-8 mt-8">
-                1. Provides time analysis data for the number of times locks and
-                unlocks occur
+                1. Provides the option to lock by dates or times later 
               </p>
-              <p className="mx-8">2. soon...</p>
-              <p className="mx-8">3. soon...</p>
+              <p className="mx-8">2. Provides the option to lock applications from the existing application list</p>
+              <p className="mx-8">3. Can set the locking time as we wish</p>
             </div>
           </div>
           <div className="w-full shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300">
             <img
               className="w-30 h-80 mx-auto mt-[-3rem] bg-white"
-              src={lockPic2}
+              src={schedulePic2}
               alt="#"
             />
             <h2 className="text-3xl font-bold text-center py-6">
-              App Lock monitoring
+              List App Scheduling
             </h2>
             <div className="text-center font-medium">
               <p className="mx-8 mt-8">
-                1. Provides actions to lock and unlock
+                1. Provides existing application data
               </p>
               <p className="mx-8">
-                2. Provides status data whether the application is in lock and
-                unlock mode
+                2. Provides application data that is currently locked via scheduling
               </p>
               <p className="mx-8">
-                3. Provides statistical data on how many times the application
-                is locked and unlocked
+                3. soon.....
               </p>
             </div>
           </div>
@@ -162,4 +156,4 @@ function DetailLock() {
   );
 }
 
-export default DetailLock;
+export default DetailScheduling;
