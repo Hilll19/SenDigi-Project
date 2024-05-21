d33ae117a83a49ff27cb9027ddbf68bd31254343
Hilll19 <hilworking0110@gmail.com>
2024-05-22 01:12:22 +0700

    test

--- src/page/Dashboard/ActivityStatus.js
+++ src/page/Dashboard/ActivityStatus.js
@@ -59,7 +59,7 @@ function ActivityStatus() {
 
   const renderActivityStatus = () => {
     return (
-      <div className="bg-gray-800 p-4 rounded-lg shadow-md overflow-y-auto max-h-102">
+      <div className="bg-gray-800 p-4 rounded-lg shadow-md overflow-y-auto max-h-96">
         <ul>
           {activityStatusList.map((activity, index) => (
             <li
